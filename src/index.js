0fa312ef569e99b8781726e2ad4eb9f1b2204ac3
Sultana Almalki <almalki.sultana@gmail.com>
2023-12-06 16:54:04 +0300

    get single product

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ const express = require("express");
 
 const app = express();
 
+let products = [
+  { id: 1, name: "apple iPhone11", price: 3500 },
+  { id: 2, name: "apple iPhone12", price: 3700 },
+  { id: 3, name: "apple iPhone13", price: 3800 },
+  { id: 4, name: "apple iPhone14", price: 4000 },
+];
+
 app.get("/", (req, res) => {
   res.send("Welcome to the express server");
 });
 
 app.get("/products", (req, res) => {
   res.send({
-    product: [
-      { id: 1, name: "apple iPhone11", price: 3500 },
-      { id: 2, name: "apple iPhone12", price: 3700 },
-      { id: 3, name: "apple iPhone13", price: 3800 },
-      { id: 4, name: "apple iPhone14", price: 4000 },
-    ],
+    products: products,
+  });
+});
+
+app.get("/products/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const product = products.find((p) => p.id === id);
+  res.send({
+    product: product,
   });
 });
 
